1c986708e06bd856406229616574ab3d3f8df0e1
metzner <unconfigured@null.spigotmc.org>
2022-05-16 16:06:06 +0200

    Initial commit

---
+++ scripts/init.lua
@@ -0,0 +1,11 @@
+ScriptHost:LoadScript("scripts/logic.lua")
+
+Tracker:AddItems("items/items.json")
+
+Tracker:AddMaps("maps/maps.json")
+
+Tracker:AddLocations("locations/logic.json")
+Tracker:AddLocations("locations/locations.json")
+
+Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/broadcast.json")
