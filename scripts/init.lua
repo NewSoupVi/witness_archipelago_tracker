4d80f14be4f674d63d3c82ef9a229e5d0c425074
Beaker271 <hocusponens@gmail.com>
2022-07-20 12:03:05 -0700

    Preliminary work

--- scripts/init.lua
+++ scripts/init.lua
@@ -7,6 +7,7 @@ Tracker:AddMaps("maps/maps.json")
 
 Tracker:AddLocations("locations/logic.json")
 Tracker:AddLocations("locations/locations.json")
+Tracker:AddLocations("locations/paths.json")
 
 Tracker:AddLayouts("layouts/tracker.json")
 Tracker:AddLayouts("layouts/broadcast.json")
