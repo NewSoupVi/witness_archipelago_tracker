a8064ad65579c18730e240ba76b8f7c870adab75
StripesOO7 <54711792+StripesOO7@users.noreply.github.com>
2023-12-05 23:50:34 +0100

    - moved require function outside of canSolve() for massive speedup - other minor changes for beeter readability and maybe speedup

--- scripts/logic.lua
+++ scripts/logic.lua
@@ -31,7 +31,7 @@ function longBoxWithoutMountainEntry()
 end
 
 function isNotDisabledByPostgame(id)
-	return not getDisabledDict()[tonumber(id)]
+	return not disabledDict[tonumber(id)]
 end
 
 function extraChecksRequired()
@@ -61,7 +61,7 @@ function laserBox(box)
 end
 
 function hasPanel(panel)
-	if Tracker:ProviderCountForCode("doorsNo") + Tracker:ProviderCountForCode("doorsDoor") > 0 then return true
+	if isNotDoors() then return true
 	else return Tracker:ProviderCountForCode(panel)
 	end
 end
@@ -97,6 +97,20 @@ function pp2()
 
 end
 
+symbolCheck = {
+	["Black/White Squares"] = "BWSquare",
+	["Colored Squares"] = "ColoredSquares",
+	["Symmetry"] = "Symmetry",
+	["Colored Dots"] = "ColoredDots",
+	["Sound Dots"] = "SoundDots",
+	["Shapers"] = "Shapers",
+	["Rotated Shapers"] = "RotatedShapers",
+	["Negative Shapers"] = "NegativeShapers",
+	["Eraser"] = "Eraser",
+	["Triangles"] = "Triangles",
+	["Arrows"] = "Arrows",
+}
+
 function hasSymbol(symbol)
 	if symbol == "Dots" then
 		return dots(1)
@@ -106,34 +120,16 @@ function hasSymbol(symbol)
 		return stars(1)
 	elseif symbol == "Stars + Same Colored Symbol" then
 		return stars(2)
-	elseif symbol == "Black/White Squares" then
-		return Tracker:ProviderCountForCode("BWSquare") == 1
-	elseif symbol == "Colored Squares" then
-		return Tracker:ProviderCountForCode("ColoredSquares") == 1
-	elseif symbol == "Symmetry" then
-		return Tracker:ProviderCountForCode("Symmetry") == 1
-	elseif symbol == "Colored Dots" then
-		return Tracker:ProviderCountForCode("ColoredDots") == 1
-	elseif symbol == "Sound Dots" then
-		return Tracker:ProviderCountForCode("SoundDots") == 1
-	elseif symbol == "Shapers" then
-		return Tracker:ProviderCountForCode("Shapers") == 1
-	elseif symbol == "Rotated Shapers" then
-		return Tracker:ProviderCountForCode("RotatedShapers") == 1
-	elseif symbol == "Negative Shapers" then
-		return Tracker:ProviderCountForCode("NegativeShapers") == 1
-	elseif symbol == "Eraser" then
-		return Tracker:ProviderCountForCode("Eraser") == 1
-	elseif symbol == "Triangles" then
-		return Tracker:ProviderCountForCode("Triangles") == 1
-	elseif symbol == "Arrows" then
-		return Tracker:ProviderCountForCode("Arrows") == 1
+	elseif symbolCheck[symbol] ~= nil then
+		-- print("test")
+		return Tracker:ProviderCountForCode(symbolCheck[symbol]) == 1
 	else
 		return true
 	end
 end
 
 function getLogicFile()
+	print(test, Tracker:ProviderCountForCode("Expert"), Tracker:ProviderCountForCode("randomizationSigma"))
 	if Tracker:ProviderCountForCode("Expert") == 1 then
 		return "WitnessLogicExpert"
 	elseif Tracker:ProviderCountForCode("randomizationSigma") == 1 then
@@ -156,15 +152,15 @@ function parseIds(ids)
 	end
 end
 
-
+require(getLogicFile())
 function canSolve(ids)
 	if Tracker:ProviderCountForCode("Symbols") == 0 or isClearing() then
 		return true
 	end
 	ids = parseIds(ids)
-	require(getLogicFile())
+	-- require("WitnessLogic")
 	for id in ids:gmatch("%S+") do
-		requiredSymbols = getLogic()[tonumber(id)]
+		requiredSymbols = panel[tonumber(id)]
 		for k, v in pairs(requiredSymbols) do
 			if(not hasSymbol(v)) then
 				return false
