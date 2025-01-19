e83f7a171074729d0a8f7b9c7c24c73690ea65bf
Exempt-Medic <ExemptMedic@Gmail.com>
2025-01-19 17:09:52 -0500

    Switch to BulkUpdate

--- scripts/archipelago.lua
+++ scripts/archipelago.lua
@@ -11,7 +11,6 @@ SLOT_DATA = nil
 LOCAL_ITEMS = {}
 GLOBAL_ITEMS = {}
 disabledDict = {}
-currentlyClearing = false
 
 lasers = {0,0,0,0,0,0,0,0,0,0,0}
 
@@ -438,7 +437,7 @@ function setReply(key, val, old)
 end
 
 function onClear(slot_data)
-	currentlyClearing = true
+	Tracker.BulkUpdate = true
 
 	lasers = {0,0,0,0,0,0,0,0,0,0,0}
 
@@ -614,7 +613,7 @@ function onClear(slot_data)
 		end
 	end
 
-	currentlyClearing = false
+	Tracker.BulkUpdate = false
 
 	Tracker:FindObjectForCode("Tutorial 1 Extra").Active,
 	Tracker:FindObjectForCode("Tutorial 2 Extra").Active,
@@ -803,10 +802,6 @@ end
 
 function laser(num)
 	return (lasers[tonumber(num)] > 0)
-end
-
-function isClearing()
-	return currentlyClearing
 end
 
 function randomizationChanged()
